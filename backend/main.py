a39a3fcde116b47526dfba873e896ed4eab14390
thlurte <s92062851@ousl.lk>
2025-04-03 23:07:06 +0530

    add: routers for auth

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,27 @@
+from fastapi.responses import RedirectResponse
 from fastapi import FastAPI
+from dotenv import dotenv_values
+from typing import Dict
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
+import time
+
+from routers import auth
 
 app = FastAPI()
 
+# routers.include
+app.include_router(auth.router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+config = dotenv_values(".env")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# auth_handler = AuthHandler()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
